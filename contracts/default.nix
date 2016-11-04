1624322af132d638373d4bd2283eba213e903a6d
Stewart Mackenzie <setori88@gmail.com>
2016-11-04 21:39:01 +0800

    Problem: example_wrangle doesn't conform to new module system
    Solution: make it conform

--- contracts/default.nix
+++ contracts/default.nix
@@ -3,5 +3,4 @@ let
 callPackage = pkgs.lib.callPackageWith (pkgs // allContracts // support);
 in
 rec {
-  vendor_maths_boolean = callPackage ./vendor/maths/boolean {};
 }
--- default.nix
+++ default.nix
@@ -2,16 +2,24 @@
   , pkgs ? fractalide.pkgs
   , support ? fractalide.support
   , contracts ? fractalide.contracts
-  , components ? fractalide.components}:
+  , components ? fractalide.components
+  , fractalide_user ? null
+  , config_file ? null}:
 let
-  ### change only these two expressions
-  publicComponentOrSubnet = allComponents.example_wrangle; # expose your public reusable subnet
-  exeSubnet = allComponents.example_wrangle; # a top level subnet containing non-generic IIPs you don't want to expose to the community
-  allContracts = contracts // import ./contracts {inherit pkgs support allContracts;};
-  allComponents = components // import ./components {inherit pkgs support allContracts allComponents;};
+  exeSubnet = fractalComponents.example_wrangle; 
+  publicNamespace = { components = fractalComponents; contracts = fractalContracts; };
+  fractalContracts = import ./contracts {inherit pkgs support allContracts;};
+  fractalComponents = import ./components {inherit pkgs support allContracts allComponents;};
+  allContracts = contracts // fractalContracts;
+  allComponents = components // fractalComponents // { encrypt = encrypt; };
+  encrypt = support.encryptComponent {
+      fractalide_user = fractalide_user;
+      keybase_config_file = config_file; };
+  result = if fractalide == null
+    then publicNamespace
+    else import (<fractalide> + "/support/vm/") {inherit pkgs support;
+      contracts = allContracts;
+      components = allComponents;
+      exeSubnet = exeSubnet;};
 in
-if fractalide == null then publicComponentOrSubnet
-else import (<fractalide> + "/support/vm/") {inherit pkgs support;
-  contracts = allContracts;
-  components = allComponents;
-  exeSubnet = exeSubnet;}
+  result
